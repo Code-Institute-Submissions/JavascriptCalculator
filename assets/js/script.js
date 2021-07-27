37d4f7a6535e41c7d085202285b90e5f6a5f3fe6
TechCentreUK <support@techcentreuk.co.uk>
2021-07-27 19:14:24 +0000

    Create Variables in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Call all data-number attributes and create a variable
+const numberButtons = document.querySelectorAll('[data-number]'),
+// Call all data-operator attributes and create a variable
+operatorButtons = document.querySelectorAll('[data-operator]'),
+// Call data-equals attribute and create a variable
+equalsButton = document.querySelector('[data-equals]'),
+// Call the Delete/Clear (DEL Button) attribute and create a variable
+deleteButton = document.querySelector('[data-delete]'),
+// Call the data-all-clear (AC Button) attribute and create a variable
+allClearButton = document.querySelector('[data-all-clear]'),
+// Call the data-previous-operand attribute and create a variable
+previousOperand = document.querySelector('[data-previous-operand]'),
+// Call the data-current-operand
+currentOperand = document.querySelector('[data-current-operand]');
