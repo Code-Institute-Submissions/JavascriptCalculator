6b4ede13b760f82064ca1931bf20f71ab22b1147
TechCentreUK <support@techcentreuk.co.uk>
2021-07-28 00:41:55 +0000

    Add styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ class Calculator {
     // Clear Calculator Function
     clear() {
         this.previousOperand = '';
-        this.currentOperand = '';
+        this.currentOperand = '0';
         this.operator = '';
     }
 
